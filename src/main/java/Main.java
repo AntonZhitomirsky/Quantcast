d40e277dad65980e10c9160a24ef53b456ed3284
Anton Zhitomirsky <anton.zhitomirsky@gmail.com>
2022-05-25 17:36:35 +0100

    Main ties all classes together into functioning code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,27 @@
-import java.util.Arrays;
+import entry.TimeStampedCookieEntry;
+import file.CookieFile;
+import file.CookieFileInterface;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.List;
+import parser.StringParser;
+import search.ConcurrentSearch;
 
 public class Main {
 
-  public static void main(String[] args) {
-    System.out.println(Arrays.toString(args));
-  }
+  public static void main(String[] args) throws ParseException {
+    // minimal checking for command line inputs!
+    String file = args[0];
+    assert args[1].equals("-d");
+    String date = args[2];
 
+    CookieFileInterface<TimeStampedCookieEntry> cookieFile = new CookieFile<>(new StringParser<>(),
+        file);
+    ConcurrentSearch search = new ConcurrentSearch(cookieFile);
+    List<String> cookies = search.getMostFrequentCookie(
+        new SimpleDateFormat("yyyy-MM-dd").parse(date));
+
+    // pretty print output
+    cookies.forEach(System.out::println);
+  }
 }
